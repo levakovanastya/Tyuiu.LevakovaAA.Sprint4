af359cf17ae76f4bdfb433be1b89c8c2a75166b7
Настя Левакова <tyuiu.levakovaaa@outlook.com>
2025-11-13 20:55:37 +0500

    Task4

---
+++ Tyuiu.LevakovaAA.Sprint4.Task4.V25.Test/DataServiceTest.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.LevakovaAA.Sprint4.Task4.V25.Lib;
+using System;
+namespace Tyuiu.LevakovaAA.Sprint4.Task4.V25.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidCalc()
+        {
+            DataService ds = new DataService();
+            int[,] mas = new int[5, 5] { { 3, 2, 4, 4, 4 }, { 2, 6, 6, 3, 2 }, { 4, 2, 5, 5, 3 }, { 5, 5, 6, 4, 3 }, { 2, 6, 5, 6, 2 } };
+            int res = ds.Calculate(mas);
+            Assert.AreEqual(62, res);
+        }
+    }
+}
---
+++ Tyuiu.LevakovaAA.Sprint4.Task4.V25/Program.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.LevakovaAA.Sprint4.Task4.V25.Lib;
+namespace Tyuiu.LevakovaAA.Sprint4.Task4.V25
+{
+    class Programm
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #4 | Выполнила: Левакова А.А. | СМАРТб-25-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #4                                                               *");
+            Console.WriteLine("* Одномерные массивы (ввод с клавиатуры)                                  *");
+            Console.WriteLine("* Задание 4                                                               *");
+            Console.WriteLine("* Вариант #25                                                             *");
+            Console.WriteLine("* Выполнила: Левакова А.А. | СМАРТб-25-1                                  *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Спринт #4                                                               *");
+            Console.WriteLine("* Дан двумерный целочисленный массив 5 на 5 элементов, заполненный        *");
+            Console.WriteLine("* значениями с клавиатуры в диапазоне от 2 до 6. Найдите сумму четных     *");
+            Console.WriteLine("* элементов массива.                                                      *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.Write("Введите количество строк в массиве: ");
+            int rows = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Введите количество столбцов в массиве: ");
+            int columns = Convert.ToInt32(Console.ReadLine());
+
+            int[,] mtrx = new int[rows, columns];
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("Исходный массив:");
+
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j  = 0; j < columns; j++)
+                {
+                    Console.WriteLine($"Введите значение {i},{j} элемента массива: ");
+                    mtrx[i, j] = Convert.ToInt32(Console.ReadLine());
+                }
+            }
+
+            Console.WriteLine("\nМассив:");
+
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0;j < columns; j++)
+                {
+                    Console.WriteLine($"{mtrx[i, j]} \t");
+                }
+                Console.WriteLine();
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            int res = ds.Calculate(mtrx);
+
+            Console.WriteLine(res);
+            Console.ReadKey();
+        }
+    }
+}
+
