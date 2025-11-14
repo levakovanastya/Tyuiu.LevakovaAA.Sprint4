896ec543800821e270b7e160c2f6885444832a8d
Настя Левакова <tyuiu.levakovaaa@outlook.com>
2025-11-14 17:02:03 +0500

    Task7

---
+++ Tyuiu.LevakovaAA.Sprint4.Task7.V22.Test/DataServiceTest.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.LevakovaAA.Sprint4.Task7.V22.Lib;
+using System;
+namespace Tyuiu.LevakovaAA.Sprint4.Task7.V22.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidCalc()
+        {
+            int rows = 3;
+            int columns = 5;
+            int[,] mtrx = new int[rows, columns];
+            string str = "695324951753684";
+            DataService ds = new DataService();
+            int res = ds.Calculate(rows, columns, str);
+            Assert.AreEqual(9216, res);
+        }
+    }
+}
---
+++ Tyuiu.LevakovaAA.Sprint4.Task7.V22/Program.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.LevakovaAA.Sprint4.Task7.V22.Lib;
+namespace Tyuiu.LevakovaAA.Sprint4.Task7.V22
+{
+    class Programm
+    {
+        static void Main(string[] args)
+        {
+            int rows = 3;
+            int columns = 5;
+            int[,] mtrx = new int[rows, columns];
+            string str = "695324951753684";
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #4 | Выполнила: Левакова А.А. | СМАРТб-25-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #4                                                               *");
+            Console.WriteLine("* Добавление к решению итоговых проектов по спринту                       *");
+            Console.WriteLine("* Задание 7                                                               *");
+            Console.WriteLine("* Вариант #22                                                             *");
+            Console.WriteLine("* Выполнила: Левакова А.А. | СМАРТб-25-1                                  *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Спринт #4                                                               *");
+            Console.WriteLine("* Дана строка из одноразрядных цифр 695324951753684. Преобразуйте ее в    *");
+            Console.WriteLine("* матрицу 5 на 3 и подсчитайте произведение четных чисел.                 *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            int index = 0;
+
+            Console.WriteLine("\nМассив:");
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < columns; j++)
+                {
+                    Console.Write($"{str[index]} \t");
+                    index++;
+                }
+                Console.WriteLine();
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            int res = ds.Calculate(rows, columns, str);
+
+            Console.WriteLine("Произведение четных чисел = " + res);
+            Console.ReadKey();
+        }
+    }
+}
+
